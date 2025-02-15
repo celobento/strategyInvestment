6f50d3f7c52f3a72ba67ce22cd17c2b4d6fb5d57
Marcelo Bento <celobento26@gmail.com>
2025-02-15 10:13:22 -0300

    first commit

---
+++ backend/src/main/java/br/com/systemit/strategyInvestment/StrategyInvestmentApplication.java
@@ -0,0 +1,13 @@
+package br.com.systemit.strategyInvestment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StrategyInvestmentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StrategyInvestmentApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/br/com/systemit/strategyInvestment/StrategyInvestmentApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.systemit.strategyInvestment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StrategyInvestmentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
