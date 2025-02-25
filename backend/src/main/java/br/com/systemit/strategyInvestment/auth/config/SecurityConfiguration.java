40055fcd3e0b4f6bb09acc05e30b71d1b7aae238
Marcelo Bento <celobento26@gmail.com>
2025-02-25 15:50:54 -0300

    config access free to swagger

--- backend/src/main/java/br/com/systemit/strategyInvestment/auth/config/SecurityConfiguration.java
+++ backend/src/main/java/br/com/systemit/strategyInvestment/auth/config/SecurityConfiguration.java
@@ -9,6 +9,7 @@ import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.core.GrantedAuthorityDefaults;
 import org.springframework.security.core.userdetails.UserDetailsService;
@@ -45,6 +46,21 @@ public class SecurityConfiguration {
     @Bean
     public UserDetailsService userDetailsService(UserService userService) {
         return new CustomUserDetailService(userService);
+    }
+
+    @Bean
+    public WebSecurityCustomizer webSecutiryCustomizer() {
+        return web -> {
+            web.ignoring().requestMatchers(
+                    "/v2/api-docs/**",
+                    "/v3/api-docs/**",
+                    "/swagger-resources/**",
+                    "/swagger-ui.html",
+                    "/swagger-ui/**",
+                    "/webjars/**",
+                    "/actuator/**"
+            );
+        };
     }
 
     // removing prefix ROLE_
